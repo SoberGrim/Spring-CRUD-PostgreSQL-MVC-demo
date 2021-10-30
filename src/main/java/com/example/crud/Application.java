d733b0443dabb4c9b98a2510207f7e7022e1baf5
Александр Моисеев <sobergrim@gmail.com>
2021-10-30 11:19:55 +0300

    v 6.6

---
+++ src/main/java/com/example/crud/Application.java
@@ -0,0 +1,21 @@
+package com.example.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import java.io.IOException;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class Application {
+
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(Application.class, args);
+        openHomePage();
+    }
+    private static void openHomePage() throws IOException {
+        Runtime rt = Runtime.getRuntime();
+        rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost/");
+    }
+}
--- src/main/java/ru/jm/crud/model/HTTPRequest.java
+++
@@ -1,7 +0,0 @@
-package ru.jm.crud.model;
-
-public class HTTPRequest {
-    public String url;
-    public String method;
-    public String postData;
-}
